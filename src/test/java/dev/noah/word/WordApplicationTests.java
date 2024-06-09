f7a905a3ec82add3d9ba8bf92a19093c4bf2b88b
49EHyeon42 <49ehyeon42@gmail.com>
2024-06-09 09:39:47 +0900

    init: 프로젝트 시작

---
+++ src/test/java/dev/noah/word/WordApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.noah.word;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WordApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
