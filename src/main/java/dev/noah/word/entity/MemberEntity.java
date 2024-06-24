6b65953b1afd3fb9371f8bb94cdf1bbdcd8c9e92
49EHyeon42 <49ehyeon42@gmail.com>
2024-06-24 10:48:43 +0900

    squash-merge: 영속성 전이 명시
    commit f85291821b6fec7af08a151b967be36f703b349b
    Author: 49EHyeon42 <49ehyeon42@gmail.com>
    Date:   Mon Jun 24 10:45:23 2024 +0900
    
        feat: 계정 삭제 시 게시글, 댓글을 삭제할 수 있도록 영속성 전시 설정, @Getter(AccessLevel.NONE)로 은닉
    
    commit 7e28391132111d8be02ed64bf4c2ea9a33df52d2
    Author: 49EHyeon42 <49ehyeon42@gmail.com>
    Date:   Mon Jun 24 10:34:32 2024 +0900
    
        feat: 게시글 삭제 시 댓글을 삭제할 수 있도록 영속성 전이 설정, @Getter(AccessLevel.NONE) 명시로 은닉

--- src/main/java/dev/noah/word/entity/MemberEntity.java
+++ src/main/java/dev/noah/word/entity/MemberEntity.java
@@ -5,6 +5,8 @@ import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 @Entity
 @Table(name = "member")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -26,4 +28,12 @@ public class MemberEntity {
 
     @Column(nullable = false, unique = true, length = 10)
     private String nickname;
+
+    @OneToMany(mappedBy = "memberEntity", fetch = FetchType.LAZY, cascade = CascadeType.REMOVE, orphanRemoval = true)
+    @Getter(AccessLevel.NONE)
+    private List<PostEntity> postEntities;
+
+    @OneToMany(mappedBy = "memberEntity", fetch = FetchType.LAZY, cascade = CascadeType.REMOVE, orphanRemoval = true)
+    @Getter(AccessLevel.NONE)
+    private List<CommentEntity> commentEntities;
 }
--- src/main/java/dev/noah/word/entity/PostEntity.java
+++ src/main/java/dev/noah/word/entity/PostEntity.java
@@ -8,6 +8,7 @@ import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Entity
 @Table(name = "post")
@@ -41,6 +42,10 @@ public class PostEntity {
 
     @Column(columnDefinition = "integer default 0", nullable = false)
     private int likes;
+
+    @OneToMany(mappedBy = "postEntity", fetch = FetchType.LAZY, cascade = CascadeType.REMOVE, orphanRemoval = true)
+    @Getter(AccessLevel.NONE)
+    private List<CommentEntity> commentEntities;
 
     public PostEntity(MemberEntity memberEntity, String imageUrl, String title, String content) {
         this.memberEntity = memberEntity;
